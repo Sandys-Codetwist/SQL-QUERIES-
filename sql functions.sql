96cade8bc51ff7f9b2e2bd8dd0ed9d3c11f019bc
Codetwist <99180218+Sandys-Codetwist@users.noreply.github.com>
2022-02-26 11:30:42 +0530

    Add files via upload

---
+++ sql functions.sql
@@ -0,0 +1,6 @@
+use org;
+select min(s_marks) from student1;
+select max(s_marks) from student1;
+select sum(s_marks) from student1;
+select count(s_marks) from student1;
+select avg(s_marks) from student1;
